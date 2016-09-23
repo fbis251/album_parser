4f15c708a00a9ebb5f9133abc9ca1eacac4a76da
Fernando Barillas <fbis251@mailbox.org>
2016-09-22 20:26:38 -0700

    Add InvalidApiKeyException
    * Used for APIs that require an API key to query their service. Will
      be thrown if the library user forgot to set their API key when
      instantiating the library

---
+++ lib/src/main/java/com/fernandobarillas/albumparser/exception/InvalidApiKeyException.java
@@ -0,0 +1,46 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2016 Fernando Barillas (FBis251)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.fernandobarillas.albumparser.exception;
+
+import java.net.URL;
+
+/**
+ * This Exception is thrown when the library is unable to parse a particular URL and is thus
+ * unsupported Created by fb on 5/13/16.
+ */
+public class InvalidApiKeyException extends IllegalArgumentException {
+    private static final String message = "Invalid API key set";
+
+    public InvalidApiKeyException(URL url, String apiKey, String errorMessage) {
+        super(message
+                + ": url = ["
+                + url
+                + "], apiKey = ["
+                + apiKey
+                + "], errorMessage = ["
+                + errorMessage
+                + "]");
+    }
+
+    public InvalidApiKeyException(URL url, String apiKey) {
+        super(message + ": url = [" + url + "], apiKey = [" + apiKey + "]");
+    }
+}
