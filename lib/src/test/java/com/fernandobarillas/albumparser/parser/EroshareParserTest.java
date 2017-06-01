1e5b9110d2cc19255e7d1e00e6732efed4061e67
Fernando Barillas <fbis251@mailbox.org>
2017-06-01 08:33:37 -0700

    Eroshare: Add single media tests

--- lib/src/test/java/com/fernandobarillas/albumparser/parser/EroshareParserTest.java
+++ lib/src/test/java/com/fernandobarillas/albumparser/parser/EroshareParserTest.java
@@ -25,6 +25,7 @@ import com.fernandobarillas.albumparser.media.IMedia;
 import com.fernandobarillas.albumparser.model.ExpectedAlbum;
 import com.fernandobarillas.albumparser.model.ExpectedMedia;
 import com.fernandobarillas.albumparser.model.ExpectedParserResponse;
+import com.fernandobarillas.albumparser.util.TestUtils;
 
 import org.junit.Test;
 
@@ -35,8 +36,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import okhttp3.OkHttpClient;
-
 import static com.fernandobarillas.albumparser.util.ParseUtils.getUrlObject;
 import static com.fernandobarillas.albumparser.util.TestUtils.API_CALL_TIMEOUT_MS;
 import static com.fernandobarillas.albumparser.util.TestUtils.compareParserResponse;
@@ -46,37 +45,40 @@ import static com.fernandobarillas.albumparser.util.TestUtils.validateCanParseAn
  * Tests for Eroshare API Parser
  */
 public class EroshareParserTest {
-    private OkHttpClient   mOkHttpClient;
+
+    private static final ExpectedMedia EXPECTED_IMAGE =
+            new ExpectedMedia.Builder().setDescription("Homer Simpson Eating a Donut")
+                    .setPreviewUrl("https://i.eroshare.com/wja22gm7_thumb.jpg")
+                    .setHighQualityUrl("https://i.eroshare.com/wja22gm7_orig.png")
+                    .setLowQualityUrl("https://i.eroshare.com/wja22gm7.jpg")
+                    .setHighQualityHeight(391)
+                    .setHighQualityWidth(239)
+                    .build();
+    private static final ExpectedMedia EXPECTED_VIDEO =
+            new ExpectedMedia.Builder().setDescription("Up and Atom")
+                    .setPreviewUrl("https://i.eroshare.com/gvensf3w_thumb.jpg")
+                    .setHighQualityUrl("https://v.eroshare.com/gvensf3w.mp4")
+                    .setLowQualityUrl("https://v.eroshare.com/gvensf3w_lowres.mp4")
+                    .setHighQualityHeight(464)
+                    .setHighQualityWidth(608)
+                    .setDuration(11)
+                    .setIsVideo(true)
+                    .build();
+
     private EroshareParser mEroshareParser;
 
     public EroshareParserTest() {
-        mOkHttpClient = new OkHttpClient();
-        mEroshareParser = new EroshareParser(mOkHttpClient);
+        mEroshareParser = new EroshareParser(TestUtils.getOkHttpClient());
     }
 
-    // Tests a standard album URL with the old API call
+    // Tests a standard album URL
     @Test(timeout = API_CALL_TIMEOUT_MS)
     public void testAlbum() throws IOException, RuntimeException {
         URL albumUrl = getUrlObject("https://eroshare.com/3fdr6v4a");
         String albumPreviewUrl = "https://i.eroshare.com/wja22gm7_thumb.jpg";
         List<IMedia> expectedMediaList = new ArrayList<>();
-        expectedMediaList.add(new ExpectedMedia.Builder().setDescription(
-                "Homer Simpson Eating a Donut")
-                .setPreviewUrl("https://i.eroshare.com/wja22gm7_thumb.jpg")
-                .setHighQualityUrl("https://i.eroshare.com/wja22gm7_orig.png")
-                .setLowQualityUrl("https://i.eroshare.com/wja22gm7.jpg")
-                .setHighQualityHeight(391)
-                .setHighQualityWidth(239)
-                .build());
-        expectedMediaList.add(new ExpectedMedia.Builder().setDescription("Up and Atom")
-                .setPreviewUrl("https://i.eroshare.com/gvensf3w_thumb.jpg")
-                .setHighQualityUrl("https://v.eroshare.com/gvensf3w.mp4")
-                .setLowQualityUrl("https://v.eroshare.com/gvensf3w_lowres.mp4")
-                .setHighQualityHeight(464)
-                .setHighQualityWidth(608)
-                .setDuration(11)
-                .setIsVideo(true)
-                .build());
+        expectedMediaList.add(EXPECTED_IMAGE);
+        expectedMediaList.add(EXPECTED_VIDEO);
         expectedMediaList.add(new ExpectedMedia.Builder().setDescription("You're a Winner!")
                 .setPreviewUrl("https://i.eroshare.com/0mvd0dp7_thumb.jpg")
                 .setHighQualityUrl("https://v.eroshare.com/0mvd0dp7.mp4")
@@ -108,5 +110,29 @@ public class EroshareParserTest {
         validHashes.put("wja22gm7", "https://i.eroshare.com/wja22gm7.jpg"); // i subdomain
 
         validateCanParseAndHashes(mEroshareParser, validHashes, false);
+    }
+
+    // Tests a single image URL
+    @Test(timeout = API_CALL_TIMEOUT_MS)
+    public void testImageItem() throws IOException, RuntimeException {
+        URL imageUrl = getUrlObject("https://i.eroshare.com/wja22gm7.jpg");
+        ExpectedParserResponse expectedParserResponse = new ExpectedParserResponse.Builder(imageUrl)
+                .setIsSingleMedia(true)
+                .setMedia(EXPECTED_IMAGE)
+                .build();
+        ParserResponse parserResponse = mEroshareParser.parse(imageUrl);
+        compareParserResponse(imageUrl, expectedParserResponse, parserResponse);
+    }
+
+    // Tests a single video URL
+    @Test(timeout = API_CALL_TIMEOUT_MS)
+    public void testVideoItem() throws IOException, RuntimeException {
+        URL videoUrl = getUrlObject("https://v.eroshare.com/gvensf3w.mp4");
+        ExpectedParserResponse expectedParserResponse = new ExpectedParserResponse.Builder(videoUrl)
+                .setIsSingleMedia(true)
+                .setMedia(EXPECTED_VIDEO)
+                .build();
+        ParserResponse parserResponse = mEroshareParser.parse(videoUrl);
+        compareParserResponse(videoUrl, expectedParserResponse, parserResponse);
     }
 }
