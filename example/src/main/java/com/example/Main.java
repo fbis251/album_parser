043feec0426685d71f6813113d70211504bd7d4e
Fernando Barillas <fb@fbis251.com>
2019-12-21 00:31:55 -0800

    Version 0.15.2

--- example/src/main/java/com/example/Main.java
+++ example/src/main/java/com/example/Main.java
@@ -12,9 +12,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Starting");
         // Example to use a proxy server
-        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("192.168.1.2", 8080));
-        OkHttpClient client = new OkHttpClient.Builder().proxy(proxy).build();
-        AlbumParserTest.testAlbumParser(client);
+//        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("192.168.1.2", 8080));
+//        OkHttpClient client = new OkHttpClient.Builder().proxy(proxy).build();
+//        AlbumParserTest.testAlbumParser(client);
         // Without a proxy
         AlbumParserTest.testAlbumParser(null);
         System.out.println("Done");
